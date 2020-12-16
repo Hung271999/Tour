6c61d2978bc6e7484d111dca8dd0d7d5fb94f154
hung271999@gmail.com <hung271999@gmail.com>
2020-12-16 22:04:24 +0700

    first commit

---
+++ Tour/Tour/App_Start/BundleConfig.cs
@@ -0,0 +1,64 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Tour
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/vendor/modernizr-3.5.0.min.js",
+                        "~/js/vendor/jquery-1.12.4.min.js",
+                        "~/js/popper.min.js",
+                        "~/js/bootstrap.min.js",
+                        "~/js/owl.carousel.min.js",
+                        "~/js/isotope.pkgd.min.js",
+                        "~/js/ajax-form.js",
+                        "~/js/waypoints.min.js",
+                        "~/js/jquery.counterup.min.js",
+                        "~/js/imagesloaded.pkgd.min.js",
+                        "~/js/scrollIt.js",
+                        "~/js/jquery.scrollUp.min.js",
+                        "~/js/wow.min.js",
+                        "~/js/nice-select.min.js",
+                        "~/js/jquery.slicknav.min.js",
+                        "~/js/jquery.magnific-popup.min.js",
+                        "~/js/plugins.js",
+                        "~/js/contact.js",
+                        "~/js/jquery.ajaxchimp.min.js",
+                        "~/js/jquery.form.js",
+                        "~/js/mail-script.js",
+                        "~/js/main.js",
+                        "~/js/jquery.validate.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.min.css",
+                      "~/css/owl.carousel.min.css",
+                      "~/css/magnific-popup.css",
+                      "~/css/font-awesome.min.css",
+                      "~/css/themify-icons.css",
+                      "~/css/nice-select.css",
+                      "~/css/flaticon.css",
+                      "~/css/gijgo.css",
+                      "~/css/animate.css",
+                      "~/css/gijgo.css",
+                      "~/css/slick.css",
+                      "~/css/slicknav.css",
+                      "~/css/style.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Tour/Tour/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tour.Models;
+
+namespace Tour.Controllers
+{
+    public class HomeController : Controller
+    {
+        TourDataContext data = new TourDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Place()
+        {
+            var place = data.DIEMTHAMQUANs.ToList();
+            return View(place);
+        }
+        public ActionResult Tour()
+        {
+            var tours = data.TOURs.ToList();
+            return View(tours);
+        }
+
+    }
+}
